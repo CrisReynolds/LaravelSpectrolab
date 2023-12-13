40d3657b5f46d7c48ed93d0f53b44793cd66b38a
Alex Col <a.colq3.c@gmail.com>
2023-12-13 16:19:50 -0400

    reposrte consumo

--- app/Livewire/ReporteConsumo.php
+++ app/Livewire/ReporteConsumo.php
@@ -2,12 +2,85 @@
 
 namespace App\Livewire;
 
+use App\Exports\ExportConsumo;
+use App\Models\DetalleConsumo;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ReporteConsumo extends Component
 {
+    /* Filtro */
+    public $start_date, $end_date;
+    public function index()
+    {
+        return view('consumos.reporte');
+    }
     public function render()
     {
-        return view('livewire.reporte-consumo');
+        if ($this->start_date != "" && $this->end_date != "") {
+            $reporteConsumos = DetalleConsumo::join('consumos', 'consumos.id', 'detalle_consumos.consumos_id')
+            ->join('solicitantes', 'solicitantes.id', 'consumos.solicitante_id')
+            ->join('insumos', 'insumos.id', 'detalle_consumos.insumo_id')
+            ->join('unidades', 'unidades.id', 'insumos.unidad_id')
+            ->join('proveedores', 'proveedores.id', 'insumos.proveedor_id')
+            ->whereDate('fecha_consumo', '>=', $this->start_date)
+            ->whereDate('fecha_consumo', '<=', $this->end_date)
+            ->get([
+                'fecha_consumo',
+                'detalle_consumos.cantidad',
+                'unidades.unidad_ref',
+                'insumos.detalle',
+                'insumos.marca',
+                'insumos.codigo',
+                'solicitantes.solicitante_ref',
+                'consumos.num_vale_salida'
+            ]);
+            return view('livewire.reporte-compra', compact('reporteConsumos'));
+        }
+        $now = now()->format('Y-m-d');
+        $startDate = Carbon::now();
+        $then = $startDate->firstOfMonth()->format('Y-m-d');
+        //dd($then);
+        //$reporteCompras = DetalleCompra::reporte()->get();
+        $reporteConsumos = DetalleConsumo::join('consumos', 'consumos.id', 'detalle_consumos.consumos_id')
+        ->join('solicitantes', 'solicitantes.id', 'consumos.solicitante_id')
+        ->join('insumos', 'insumos.id', 'detalle_consumos.insumo_id')
+        ->join('unidades', 'unidades.id', 'insumos.unidad_id')
+        ->join('proveedores', 'proveedores.id', 'insumos.proveedor_id')
+        ->whereDate('fecha_consumo', '>=', $then)
+        ->whereDate('fecha_consumo', '<=', $now)
+        ->get([
+            'fecha_consumo',
+            'detalle_consumos.cantidad',
+            'unidades.unidad_ref',
+            'insumos.detalle',
+            'insumos.marca',
+            'insumos.codigo',
+            'solicitantes.solicitante_ref',
+            'consumos.num_vale_salida'
+        ]);
+        return view('livewire.reporte-consumo', compact('reporteConsumos'));
+    }
+    /* Export  */
+    public function export()
+    {
+        $now = now()->format('Y-m-d');
+        if ($this->start_date != "" && $this->end_date != "") {
+            return Excel::download(new ExportConsumo($this->start_date, $this->end_date),  'consumo_'. $now.'.xlsx');
+        } else {
+
+            $startDate = Carbon::now();
+            $then = $startDate->firstOfMonth()->format('Y-m-d');
+            return Excel::download(new ExportConsumo($then, $now),  'consumo_' . $now . '.xlsx');
+        }
+    }
+    public function consultar()
+    {
+        $this->start_date;
+        $this->end_date;
+        //dd(34);
+        //$this->resetPage();
     }
 }
--- resources/views/livewire/reporte-consumo.blade.php
+++ resources/views/livewire/reporte-consumo.blade.php
@@ -1,3 +1,139 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    <x-menu>
+        <div class="flex justify-center items-center">
+            <h2 class="text-4xl font-bold dark:text-black">Reporte de Consumo</h2>
+        </div>
+        <section>
+            @if (session('success'))
+                <div
+                    class="relative flex flex-col sm:flex-row sm:items-center bg-green-400 dark:bg-green-700 shadow rounded-md py-5 pl-6 pr-8 sm:pr-6 mb-3 mt-3">
+                    <div class="flex flex-row items-center border-b sm:border-b-0 w-full sm:w-auto pb-4 sm:pb-0">
+                        <div class="text-green-500" dark:text-gray-500>
+                            <svg class="w-6 sm:w-5 h-6 sm:h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                            </svg>
+                        </div>
+                        <div class="text-sm font-medium ml-3">Exito!.</div>
+                    </div>
+                    <div class="text-sm tracking-wide text-gray-500 dark:text-white mt-4 sm:mt-0 sm:ml-4">
+                        {{ session('success') }}</div>
+                    <div
+                        class="absolute sm:relative sm:top-auto sm:right-auto ml-auto right-4 top-4 text-gray-400 hover:text-gray-800 cursor-pointer">
+                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12"></path>
+                        </svg>
+                    </div>
+                </div>
+            @endif
+        </section>
+        <div class="my-4">
+
+            <form wire:submit.prevent="consultar" class="grid md:grid-cols-5 md:gap-6" autocomplete="off">
+                <a href="{{ url('/consumos') }}"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >Atrás</a>
+                <div class="mt-1">
+                    <input type="date" wire:model="start_date" id="start_date"
+                        class="dark:text-white block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        required>
+                    @error('start_date')
+                        <small>{{ $message }}</small>
+                    @enderror
+
+                </div>
+                <div class="mt-1">
+                    <input type="date" wire:model="end_date" id="end_date"
+                        class="dark:text-white block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        required>
+                    @error('end_date')
+                        <small>{{ $message }}</small>
+                    @enderror
+
+                </div>
+                <div class="mt-0">
+                    <button type="submit"
+                        class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Consultar</button>
+                </div>
+                <a wire:click="export"
+                    class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                    >Exportar</a>
+            </form>
+        </div>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-black uppercase bg-blue-300 dark:bg-gray-700 dark:text-gray-400">
+
+                    <th scope="col" class="px-6 py-3">
+                        Fecha
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Cant.
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Unidad
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Detalle
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Marca
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Codigo
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Nombre
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        No.Boleta
+                    </th>
+                </thead>
+                <tbody>
+                    @isset($reporteConsumos)
+
+
+                    @foreach ($reporteConsumos as $consumo)
+                        <tr
+                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <th scope="row"
+                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $consumo->fecha_consumo }}
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ $consumo->cantidad }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $consumo->unidad_ref }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $consumo->detalle }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $consumo->marca }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $consumo->codigo }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $consumo->solicitante_ref }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $consumo->num_vale_salida }}
+                            </td>
+
+
+                        </tr>
+                    @endforeach
+                    @endisset
+                </tbody>
+            </table>
+
+            {{-- {{ $compras->links() }} <!-- Aquí se muestra la paginación --> --}}
+        </div>
+
+    </x-menu>
 </div>
